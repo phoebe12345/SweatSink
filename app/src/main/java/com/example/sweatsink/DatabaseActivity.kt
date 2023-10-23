fcaa8bdf86638f874b45ff508e9d93b097d25139
bowerbankk <ericbowerban@gmail.com>
2023-10-22 18:08:28 -0700

    created database exercises activity

--- app/src/main/java/com/example/sweatsink/DatabaseActivity.kt
+++ app/src/main/java/com/example/sweatsink/DatabaseActivity.kt
@@ -1,11 +1,28 @@
 package com.example.sweatsink
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 
 class DatabaseActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.database_activity)
+
+        val databaseExercisesButtonClick = findViewById<Button>(R.id.database_exercises)
+        databaseExercisesButtonClick.setOnClickListener{
+            val intent = Intent(this, DatabaseExercisesActivity::class.java)
+            startActivity(intent)
+        }
+
+        //val sharedPreference =  getSharedPreferences("PREFERENCE_NAME", Context.MODE_PRIVATE)
+        /*var editor = sharedPreference.edit()
+        editor.putString("username","Anupam")
+        editor.putLong("l",100L)
+        editor.commit()*/
+        //sharedPreference.getLong("l",1L)
+        //println(sharedPreference.getString("username","defaultName"))
     }
 }
---
+++ app/src/main/java/com/example/sweatsink/DatabaseExercisesActivity.kt
@@ -0,0 +1,14 @@
+package com.example.sweatsink
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.ComponentActivity
+
+class DatabaseExercisesActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.database_exercises_activity)
+    }
+}
