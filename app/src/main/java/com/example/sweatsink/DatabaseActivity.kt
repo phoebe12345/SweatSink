30ea6b3a7e4f10bb7853c7c0bcc95542610247d5
bowerbankk <ericbowerban@gmail.com>
2023-10-23 15:21:38 -0700

    added simple file reading to database exercises activity

--- app/src/main/java/com/example/sweatsink/DatabaseActivity.kt
+++ app/src/main/java/com/example/sweatsink/DatabaseActivity.kt
@@ -1,6 +1,5 @@
 package com.example.sweatsink
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -16,13 +15,5 @@ class DatabaseActivity : ComponentActivity() {
             val intent = Intent(this, DatabaseExercisesActivity::class.java)
             startActivity(intent)
         }
-
-        //val sharedPreference =  getSharedPreferences("PREFERENCE_NAME", Context.MODE_PRIVATE)
-        /*var editor = sharedPreference.edit()
-        editor.putString("username","Anupam")
-        editor.putLong("l",100L)
-        editor.commit()*/
-        //sharedPreference.getLong("l",1L)
-        //println(sharedPreference.getString("username","defaultName"))
     }
 }
--- app/src/main/java/com/example/sweatsink/DatabaseExercisesActivity.kt
+++ app/src/main/java/com/example/sweatsink/DatabaseExercisesActivity.kt
@@ -1,14 +1,21 @@
 package com.example.sweatsink
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
+import java.io.File
 
 class DatabaseExercisesActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.database_exercises_activity)
+
+        //INTERNAL STORAGE
+        val file = File(this.filesDir,"myfile.txt")
+        //file.writeText("yo this is a file!!!!!!!!!!")
+        println(file.readText())
+
+        val content = findViewById<TextView>(R.id.textViewDatabaseExercisesContent)
+        content.text = file.readText()
     }
 }
