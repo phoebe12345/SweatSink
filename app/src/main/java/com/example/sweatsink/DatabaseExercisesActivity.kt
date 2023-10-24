4590d6e5fff90a7ccd4f324a89064c46d9c619e4
bowerbankk <ericbowerban@gmail.com>
2023-10-23 17:42:30 -0700

    added ability to save text persistently

--- app/src/main/java/com/example/sweatsink/DatabaseExercisesActivity.kt
+++ app/src/main/java/com/example/sweatsink/DatabaseExercisesActivity.kt
@@ -1,6 +1,8 @@
 package com.example.sweatsink
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import java.io.File
@@ -11,11 +13,19 @@ class DatabaseExercisesActivity : ComponentActivity() {
         setContentView(R.layout.database_exercises_activity)
 
         //INTERNAL STORAGE
-        val file = File(this.filesDir,"myfile.txt")
+        //val file = File(this.filesDir,"myfile.txt")
         //file.writeText("yo this is a file!!!!!!!!!!")
-        println(file.readText())
+        //println(file.readText())
 
+        val file = File(this.filesDir,"saved_exercises.txt")
         val content = findViewById<TextView>(R.id.textViewDatabaseExercisesContent)
         content.text = file.readText()
+
+        val databaseExercisesSubmitButtonClick = findViewById<Button>(R.id.database_exercises_submit)
+        databaseExercisesSubmitButtonClick.setOnClickListener{
+            val userInput = findViewById<EditText>(R.id.editTextDatabaseExercisesInput)
+            file.writeText(userInput.text.toString())
+            content.text = file.readText()
+        }
     }
 }
