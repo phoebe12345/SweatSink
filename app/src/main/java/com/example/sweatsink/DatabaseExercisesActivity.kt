e91054e7f2ba5e10852a4404c1052710833f704c
bowerbankk <ericbowerban@gmail.com>
2023-10-29 13:14:53 -0700

    users can save 1 to 14 exercises persistently

--- app/src/main/java/com/example/sweatsink/DatabaseExercisesActivity.kt
+++ app/src/main/java/com/example/sweatsink/DatabaseExercisesActivity.kt
@@ -30,44 +30,91 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import java.io.File
+import kotlin.math.min
 
 class DatabaseExercisesActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         var currentIndex=0
 
+        val numExercisesFile=File(this.filesDir,"num_exercises.txt")
+        //numExercisesFile.writeText("10")
+
+        fun getNumExercises():Int{
+            var numExercises=1
+            if(numExercisesFile.isFile) {
+                numExercises = numExercisesFile.readText().toInt()
+            }
+            return numExercises
+        }
+
         //setContentView(R.layout.database_exercises_activity)
-        setContent{
-            var textContent by remember { mutableStateOf( "saved data appears here...") }
-            val context= LocalContext.current
-            Column(modifier = Modifier.fillMaxSize(),verticalArrangement = Arrangement.Center,horizontalAlignment = Alignment.End){
-                //Text(text = textContent,modifier=Modifier.padding(16.dp))
-                TextField(value=textContent, onValueChange = {
-                    textContent=it
-                }, modifier=Modifier.padding(16.dp).width(200.dp).height(500.dp))
-            }
-            Row(modifier=Modifier.fillMaxSize(), verticalAlignment = Alignment.Bottom, horizontalArrangement = Arrangement.End){
-                Button(onClick={
-                    val file=File(context.filesDir,"saved_exercise_$currentIndex.txt")
-                    file.writeText(textContent)
-                }){
-                    Text("Save Changes")
+        fun setTheContent(){
+            setContent {
+                var textContent by remember { mutableStateOf("saved data appears here...") }
+                val context = LocalContext.current
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.End
+                ) {
+                    //Text(text = textContent,modifier=Modifier.padding(16.dp))
+                    TextField(value = textContent, onValueChange = {
+                        textContent = it
+                    }, modifier = Modifier.padding(16.dp).width(200.dp).height(500.dp))
                 }
-            }
-            Column {
-                Text(text = "Saved Exercises", fontSize = 34.sp)
-                for(i in 1..10) {
+                Row(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalAlignment = Alignment.Bottom,
+                    horizontalArrangement = Arrangement.End
+                ) {
                     Button(onClick = {
-                        val file = File(context.filesDir,"saved_exercise_$i.txt")
-                        textContent=file.readText()
-                        currentIndex=i
-                    }){
-                        Text("Exercise $i")
+                        val numExercises=getNumExercises()
+                        if(numExercises>0){
+                            numExercisesFile.writeText((numExercises - 1).toString())
+                        }
+                        setTheContent()
+                    }) {
+                        Text("Remove Exercise")
+                    }
+                    Button(onClick = {
+                        val numExercises = getNumExercises()
+                        if (numExercises < 14) {//max 14 exercises
+                            numExercisesFile.writeText((numExercises + 1).toString())
+                        }
+                        setTheContent()
+                    }) {
+                        Text("Add Exercise")
+                    }
+                    Button(onClick = {
+                        val file = File(context.filesDir, "saved_exercise_$currentIndex.txt")
+                        file.writeText(textContent)
+                    }) {
+                        Text("Save Changes")
+                    }
+                }
+                Column {
+                    Text(text = "Saved Exercises", fontSize = 34.sp)
+                    val numExercises = getNumExercises()
+                    for (i in 1..numExercises) {
+                        Button(onClick = {
+                            val file = File(context.filesDir, "saved_exercise_$i.txt")
+                            if (file.isFile) {
+                                textContent = file.readText()
+                            } else {
+                                textContent = ""
+                            }
+                            currentIndex = i
+                        }) {
+                            Text("Exercise $i")
+                        }
                     }
                 }
             }
         }
+        setTheContent()
 
         //INTERNAL STORAGE
         //val file = File(this.filesDir,"myfile.txt")
