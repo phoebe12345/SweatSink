7af80805e058e39f85667a60ec12f4abe719358c
Andy Liu <andyliu4002@gmail.com>
2023-10-31 00:27:19 -0700

    Added route button
    - added button for making routes
    - added way to add new markers

--- app/src/main/java/com/example/sweatsink/MapsActivity.kt
+++ app/src/main/java/com/example/sweatsink/MapsActivity.kt
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -17,12 +18,19 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.gms.maps.model.Polyline
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
     private lateinit var mMap: GoogleMap
 
     private lateinit var lastLocation: Location
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private val markers: MutableList<Marker> = ArrayList()
+    private val polylines: MutableList<Polyline> = ArrayList()
+    private var isCreatingRoute = true
 
     companion object{
         private const val LOCATION_REQUEST_CODE = 1
@@ -37,6 +45,16 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
         mapFragment!!.getMapAsync(this)
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        // Initialize the "Create Route" button
+        val createRouteButton = findViewById<Button>(R.id.buttonCreateRoute)
+        createRouteButton.setOnClickListener {
+            if (markers.size >= 2) {
+                createWalkingRoute()
+            } else {
+                Toast.makeText(this, "Add at least two markers to create a route.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -51,6 +69,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
 //        val sydney = LatLng(-34.0, 151.0)
 //        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        mMap.setOnMapClickListener { latLng ->
+            if (isCreatingRoute) {
+                addMarker(latLng)
+            }
+        }
     }
 
     private fun getCurrentLocationUser(){
@@ -72,17 +96,62 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
                 placeMarkerOnMap(currentLatLong)
                 mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLong, 7f))
 
-        }
+            }
         }
     }
 
     private fun placeMarkerOnMap(currentLatLong: LatLng){
         val markerOptions = MarkerOptions().position(currentLatLong)
         markerOptions.title("$currentLatLong")
-        mMap.addMarker(markerOptions)
+        val marker = mMap.addMarker(markerOptions)
+        markers.add(marker ?: return)
+    }
+
+    private fun addMarker(latLng: LatLng) {
+        val markerOptions = MarkerOptions().position(latLng)
+        val marker = mMap.addMarker(markerOptions)
+        markers.add(marker ?: return)
     }
 
     override fun onMarkerClick(p0: Marker) = false
+
+    private fun createWalkingRoute() {
+        val waypoints = markers.map { it.position }
+        if (waypoints.size < 2) {
+            return
+        }
+
+        // You should replace "YOUR_API_KEY" with your Google Maps API key
+        val directionsUrl = getDirectionsUrl(waypoints, "walking", BuildConfig.MAPS_API_KEY)
+
+        // Clear previous polylines
+        clearPolylines()
+
+        //use ok http to fetch a response from google
+    }
+
+    private fun getDirectionsUrl(waypoints: List<LatLng>, mode: String, apiKey: String): String {
+        val origin = waypoints.first()
+        val destination = waypoints.last()
+
+        val waypointsString = waypoints
+            .subList(1, waypoints.size - 1)
+            .joinToString("|") { "${it.latitude},${it.longitude}" }
+
+        return "https://maps.googleapis.com/maps/api/directions/json?" +
+                "origin=${origin.latitude},${origin.longitude}" +
+                "&destination=${destination.latitude},${destination.longitude}" +
+                "&waypoints=$waypointsString" +
+                "&mode=$mode" +
+                "&key=$apiKey"
+    }
+
+    private fun clearPolylines() {
+        for (polyline in polylines) {
+            polyline.remove()
+        }
+        polylines.clear()
+    }
 
 
     /**
