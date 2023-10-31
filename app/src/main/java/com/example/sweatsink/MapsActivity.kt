7858d6382338574abf95ba0c7102b9e5e8880def
Dongan-kim <58098610+Dongan-kim@users.noreply.github.com>
2023-10-30 17:59:27 -0700

    Update MapsActivity.kt
    change to show your current location

--- app/src/main/java/com/example/sweatsink/MapsActivity.kt
+++ app/src/main/java/com/example/sweatsink/MapsActivity.kt
@@ -1,7 +1,15 @@
 package com.example.sweatsink
 
+import android.content.ContentProviderClient
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.example.sweatsink.databinding.ActivityMapsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -11,20 +19,76 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
-
     private lateinit var mMap: GoogleMap
+
+    private var currentLocation: Location? = null
+    private var fusedLocationProviderClient: FusedLocationProviderClient? = null
+    private val permissionCode = 101
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_maps)
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+//        val mapFragment = supportFragmentManager
+//            .findFragmentById(R.id.map) as SupportMapFragment
+//        mapFragment.getMapAsync(this)
+//
+        getCurrentLocationUser()
     }
 
+    private fun getCurrentLocationUser(){
+        if(ActivityCompat.checkSelfPermission(
+            this,android.Manifest.permission.ACCESS_FINE_LOCATION)!=
+            PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
+                (this,android.Manifest.permission.ACCESS_COARSE_LOCATION)!=
+            PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this,arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),permissionCode)
+            return
+        }
+
+        val getLocation = fusedLocationProviderClient!!.lastLocation
+        getLocation.addOnSuccessListener{
+            location ->
+
+            if(location != null){
+                currentLocation = location
+
+//                Toast.makeText(applicationContext,currentLocation.latitude.toString() + "" +
+//                currentLocation.longitude.toString(), Toast.LENGTH_LONG).show()
+
+                val mapFragment = (supportFragmentManager
+                    .findFragmentById(R.id.map) as SupportMapFragment?)
+                mapFragment!!.getMapAsync(this)
+
+
+
+        }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+
+            permissionCode -> if(grantResults.isNotEmpty() && grantResults[0] ==
+                PackageManager.PERMISSION_GRANTED){
+
+                getCurrentLocationUser()
+            }
+        }
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -37,9 +101,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val latLng = LatLng(currentLocation!!.latitude, currentLocation!!.longitude)
+        val markerOptions = MarkerOptions().position(latLng).title("Current Location")
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,15f))
+        mMap.addMarker(markerOptions)
+
+
+//        // Add a marker in Sydney and move the camera
+//        val sydney = LatLng(-34.0, 151.0)
+//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
+
 }
