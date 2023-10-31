706a10843fdcca6925c9466d8d22832137999496
Sam Showkati <sshowkati@my.bcit.ca>
2023-10-30 23:09:11 -0700

    Added pop-up notification when selecting a date in calendar page.

--- app/src/main/java/com/example/sweatsink/CalendarActivity.kt
+++ app/src/main/java/com/example/sweatsink/CalendarActivity.kt
@@ -1,11 +1,29 @@
 package com.example.sweatsink
 
 import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 
+
 class CalendarActivity : ComponentActivity() {
+
+    var year: Int = 0
+    var month: Int = 0
+    var day: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.calendar_activity)
+        var calendarView = findViewById<CalendarView>(R.id.calendarView)
+
+        calendarView.setOnDateChangeListener{
+            calendarView, i, j, k ->
+            year = i
+            month = j
+            day = k
+
+            val message = "Selected Date $year-$month-$day"
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        }
     }
 }
