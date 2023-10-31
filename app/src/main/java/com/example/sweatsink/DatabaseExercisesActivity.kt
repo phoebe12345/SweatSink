985990f3e114fce2f47a9277be764e2ecd1e6a98
bowerbankk <ericbowerban@gmail.com>
2023-10-30 20:35:10 -0700

    Added more error messages to databse

--- app/src/main/java/com/example/sweatsink/DatabaseExercisesActivity.kt
+++ app/src/main/java/com/example/sweatsink/DatabaseExercisesActivity.kt
@@ -1,9 +1,8 @@
 package com.example.sweatsink
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.os.Environment
+import android.os.StatFs
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -18,7 +17,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -26,17 +24,17 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import java.io.File
-import kotlin.math.min
+
 
 class DatabaseExercisesActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
 
         var currentIndex=0
 
@@ -54,17 +52,30 @@ class DatabaseExercisesActivity : ComponentActivity() {
         //setContentView(R.layout.database_exercises_activity)
         fun setTheContent(){
             setContent {
-                var textContent by remember { mutableStateOf("saved data appears here...") }
+                var textContent by remember { mutableStateOf("Saved data appears here...") }
+                if(getNumExercises()==0){
+                    textContent="You don't have anything saved, try adding an exercise..."
+                }
+
                 val context = LocalContext.current
+
+                val statFs = StatFs(Environment.getRootDirectory().absolutePath)
+                val freeBytes = (statFs.blockSizeLong * statFs.availableBlocksLong)
+                if(freeBytes<3000){
+                    Toast.makeText(context,"You're running low on storage space!",Toast.LENGTH_SHORT).show()
+                }
+
                 Column(
                     modifier = Modifier.fillMaxSize(),
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.End
                 ) {
-                    //Text(text = textContent,modifier=Modifier.padding(16.dp))
                     TextField(value = textContent, onValueChange = {
                         textContent = it
-                    }, modifier = Modifier.padding(16.dp).width(200.dp).height(500.dp))
+                    }, modifier = Modifier
+                        .padding(16.dp)
+                        .width(200.dp)
+                        .height(500.dp))
                 }
                 Row(
                     modifier = Modifier.fillMaxSize(),
