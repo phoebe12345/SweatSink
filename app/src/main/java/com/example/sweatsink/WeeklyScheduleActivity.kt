c2556da505482634a6e1784982e86280f0e74215
bowerbankk <ericbowerban@gmail.com>
2023-11-09 14:08:35 -0800

    fixed weekly schedule on new devices

--- app/src/main/java/com/example/sweatsink/WeeklyScheduleActivity.kt
+++ app/src/main/java/com/example/sweatsink/WeeklyScheduleActivity.kt
@@ -23,31 +23,29 @@ class WeeklyScheduleActivity : ComponentActivity() {
         val currentDay: Int = calendar.get(Calendar.DAY_OF_WEEK)
 
         val file = File(this.filesDir,"weekly_schedule.txt")
-        val weekPlan=WeekPlan(file.readText())
-
-        /*val weeklyScheduleTextView=findViewById<TextView>(R.id.textViewNewWeeklyScheduleContent)
-        weeklyScheduleTextView.text=weekPlan.toString()*/
-
-        val parentLayout=findViewById<View>(R.id.weeklyScheduleContentLayout) as LinearLayout
-        for(i in 0..6){
-            val dayPlan=weekPlan.days[i]
-            if (dayPlan != null) {
-                val dayText=TextView(this)
-                dayText.text=weekPlan.getDayAsString(i)
-                if((i+2)%7==currentDay) {//colour the current day black and make it bold
-                    dayText.setTextColor(Color.BLACK)
-                    dayText.setTypeface(null, Typeface.BOLD)
-                }
-                parentLayout.addView(dayText)
-                if(dayPlan.isRest){
-                    val restText=TextView(this)
-                    restText.text="rest day"
-                    parentLayout.addView(restText)
-                }else {
-                    for (j in 0 until dayPlan.workouts.size) {
-                        val checkBox = CheckBox(this)
-                        checkBox.text = dayPlan.workouts[j].toString()
-                        parentLayout.addView(checkBox)
+        if(file.isFile){
+            val weekPlan=WeekPlan(file.readText())
+            val parentLayout = findViewById<View>(R.id.weeklyScheduleContentLayout) as LinearLayout
+            for (i in 0..6) {
+                val dayPlan = weekPlan.days[i]
+                if (dayPlan != null) {
+                    val dayText = TextView(this)
+                    dayText.text = weekPlan.getDayAsString(i)
+                    if ((i + 2) % 7 == currentDay) {//colour the current day black and make it bold
+                        dayText.setTextColor(Color.BLACK)
+                        dayText.setTypeface(null, Typeface.BOLD)
+                    }
+                    parentLayout.addView(dayText)
+                    if (dayPlan.isRest) {
+                        val restText = TextView(this)
+                        restText.text = "rest day"
+                        parentLayout.addView(restText)
+                    } else {
+                        for (j in 0 until dayPlan.workouts.size) {
+                            val checkBox = CheckBox(this)
+                            checkBox.text = dayPlan.workouts[j].toString()
+                            parentLayout.addView(checkBox)
+                        }
                     }
                 }
             }
